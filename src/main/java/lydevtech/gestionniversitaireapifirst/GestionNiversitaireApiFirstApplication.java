7039be547761894e177a99744d396da2559e195c
Abdoulaye Ly <abdoulayely148@gmail.com>
2026-07-06 21:15:58 +0000

    feat: initialize spring boot project
    Set up the initial project structure including Maven wrapper, Spring Boot
    application, configuration, and OpenAPI specification.

---
+++ src/main/java/lydevtech/gestionniversitaireapifirst/GestionNiversitaireApiFirstApplication.java
@@ -0,0 +1,13 @@
+package lydevtech.gestionniversitaireapifirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionNiversitaireApiFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionNiversitaireApiFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lydevtech/gestionniversitaireapifirst/GestionNiversitaireApiFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package lydevtech.gestionniversitaireapifirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionNiversitaireApiFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
